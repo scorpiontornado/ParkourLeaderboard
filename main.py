ac69fe77e1f34942be820428973dd8cdcdcff7b6
Nicholas Langford <40455550+scorpiontornado@users.noreply.github.com>
2022-02-01 09:52:01 +0000

    Started setting up the Flask website, made a basic function to convert from minutes, seconds, millis to millis. Began coding file input.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask('app')
 
+def to_millis(minutes, seconds, millis):
+  millis_out = (minutes * 60 + seconds) *1000 + millis
+  
+  return millis_out
+
+'''
+[
+  {"name": "[First] [Last]", "raw_time": "minutes:seconds.millis", "millis": integer}
+]
+'''
+
 @app.route('/')
-def hello_world():
-  return '<h1>Hello, World!!</h1>'
+def home():
+    leaderboard = []
+    return render_template('home.html', leaderboard=leaderboard)
 
 app.run(host='0.0.0.0', port=8080)
---
+++ test.py
@@ -0,0 +1,8 @@
+with open("input.txt") as file: # Use file to refer to the file object
+
+   data = file.read()
+
+
+for line in (data):
+  print("line")
+print(data)
